d72fb822d9878e9e0d430683062dcb0b1b494dce
hiroto <hiroto@hirotonoMacBook-Pro.local>
2019-03-18 22:16:00 +0900

    refctoring

--- src/components/OffenceShow.vue
+++ src/components/OffenceShow.vue
@@ -13,10 +13,22 @@
                     class="col-xs" >
                 </vuejs-datepicker>
             </div>
-            <button @click="changeTabu(new Date(new Date().getFullYear(), -12, 1), new Date(new Date().getFullYear(), 11, 31))" :class="{'active': startAt === '2018/01/01'}" class="btn btn-outline-success btn-sm col-xs-3">トータル</button>
-            <button @click="changeTabu(new Date(new Date().getFullYear(), 0, 1), new Date(new Date().getFullYear(), 11, 31))" :class="{'active': startAt === '2019/01/01'}" class="btn btn-outline-success btn-sm col-xs-3">今シーズン</button>
-            <button @click="regulation()" :class="{'active': regurstion === true}" class="btn btn-outline-success btn-sm col-xs-3">規定打席以上</button>
-            <button @click="getData(20)" class="btn btn-outline-success btn-sm col-xs-3">直近20打席</button>
+            <button
+                @click="changeTabu(new Date(new Date().getFullYear(), -12, 1), new Date(new Date().getFullYear(), 11, 31))"
+                :class="{'active': startAt === new Date(new Date().getFullYear(), 11, 31)}"
+                class="btn btn-outline-success btn-sm col-xs-3">トータル</button>
+            <button
+                @click="changeTabu(new Date(new Date().getFullYear(), 0, 1), new Date(new Date().getFullYear(), 11, 31))"
+                :class="{'active': startAt === new Date(new Date().getFullYear(), 0, 1)}"
+                class="btn btn-outline-success btn-sm col-xs-3">今シーズン</button>
+            <button
+                @click="regulation()"
+                :class="{'active': isRegulation === true}"
+                class="btn btn-outline-success btn-sm col-xs-3">規定({{ regulationNum }}打席)以上</button>
+            <button
+                @click="recent20"
+                :class="{'active': isRecent20 === true}"
+                class="btn btn-outline-success btn-sm col-xs-3">直近20打席</button>
         </div>
         <b-table :items="showData" :fields="columns" :sort-by.sync="sortBy" :sort-desc.sync="sortDesc" striped hover responsive small />
 
@@ -133,6 +145,8 @@ export default {
     },
     name: "offence-show",
     mounted() {
+        // TODO:デフォルトで「今シーズン」選択
+        // this.startAt = new Date(new Date().getFullYear(), 0, 1)
         this.getData(1000)
     },
     data() {
@@ -145,7 +159,9 @@ export default {
             startAt: new Date(new Date().getFullYear(), 0, 1),
             endAt: new Date(new Date().getFullYear(), 11, 31),
             selectDate: "",
-            regurstion: false,
+            isRegulation: false,
+            regulationNum: 0,
+            isRecent20: false,
             highlighted: {
                 dates: [//TODO: 動的に取得
                     new Date("2019/02/03"),
@@ -162,10 +178,28 @@ export default {
             return moment(date).format('YYYY/MM/DD');
         },
         changeTabu: function (startAt, endAt) {
-            this.regurstion = false
+            this.isRegulation = false
+            this.isRecent20 = false
+            this.regulationNum = 0
             this.startAt = startAt
             this.endAt = endAt ? endAt : startAt
             this.getData(1000)
+        },
+        regulation: function() {
+            this.isRegulation = true
+            this.showData = [0]
+            for (let i = 0; i < this.mainData.length; i++) {
+                if (this.mainData[i]["打席数"] >= this.regulationNum) {
+                    this.showData[i] = this.mainData[i]
+                }
+            }
+            this.showData = this.showData.filter(v => v)
+            this.showData = this.statistic(this.showData)
+        },
+        recent20: function() {
+            this.isRegulation = false
+            this.isRecent20 = true
+            this.getData(20)
         },
         getData: function(filterNum){
             const directory = '/records'
@@ -192,200 +226,181 @@ export default {
                 }, [])
                 this.mainData = sumOffenceData
                 // console.log(sumOffenceData)
-                this.mainData = statistic(sumOffenceData)
+                this.mainData = this.statistic(sumOffenceData)
                 this.showData = this.mainData
             })
         },
-        regulation: function() {
-            this.regurstion = true
-            const maxData = findMax(this.mainData)
-            const regulation = maxData['試合'] * 2
-            this.showData = [0]
-            for (let i = 0; i < this.mainData.length; i++) {
-                if (this.mainData[i]["打席数"] >= regulation) {
-                    this.showData[i] = this.mainData[i]
+        statistic: function(mainData) {
+            for (let i = 0; i < mainData.length; i++) {
+                const dasu = mainData[i]["打数"]
+                const shishi = mainData[i]["四球"] + mainData[i]["死球"]
+                mainData[i]["安打"] = mainData[i]["1塁打"] + mainData[i]["2塁打"] + mainData[i]["3塁打"] + mainData[i]["本塁打"]
+                mainData[i]["塁打数"] = mainData[i]["1塁打"] + mainData[i]["2塁打"] * 2 + mainData[i]["3塁打"] * 3 + mainData[i]["本塁打"]* 4
+                mainData[i]["打率"] = (mainData[i]["安打"] / dasu).toFixed(3)
+                mainData[i]["三割(4打数)"] = this.calculate_tree_ratio(mainData[i]["安打"], dasu)
+                const x = (mainData[i]["安打"] + shishi) / (dasu + shishi + mainData[i]["犠飛"] + mainData[i]["犠打"])
+                mainData[i]["出塁率"] = Math.floor(x * 100) / 100
+                mainData[i]["長打率"] = Math.floor(mainData[i]["塁打数"] / dasu * 100) / 100
+                mainData[i]["OPS"] = (mainData[i]["長打率"] + mainData[i]["出塁率"]).toFixed(3)
+                mainData[i]["BB/K"] = (shishi / mainData[i]["三振"]).toFixed(3)
+                mainData[i]["XR"] = (
+                    mainData[i]["1塁打"]
+                    + mainData[i]["2塁打"] * 1.44
+                    + mainData[i]["3塁打"] * 2.08
+                    + mainData[i]["本塁打"] * 2.88
+                    + shishi * 0.68
+                    + mainData[i]["盗塁"] * 0.36
+                    + (dasu - mainData[i]["安打"] - mainData[i]["三振"]) * (-0.18)
+                    + mainData[i]["三振"] * (-0.196)
+                    + mainData[i]["併殺打"] * (-0.74)
+                    + mainData[i]["犠飛"] * 0.74
+                    + mainData[i]["犠打"] * 0.08
+                    + mainData[i]["牽制死"] * (-0.64)
+                ).toFixed(2)
+
+                mainData[i]["XR27"] = (
+                    mainData[i]["XR"] /
+                    (dasu -
+                    mainData[i]["安打"] +
+                    mainData[i]["牽制死"] +
+                    mainData[i]["犠飛"] +
+                    mainData[i]["犠打"] +
+                    mainData[i]["併殺打"]) * 27
+                ).toFixed(2)
+
+                mainData[i]["WOBA"] = (
+                    0.692 * mainData[i]["四球"] +
+                    0.73 * mainData[i]["死球"] +
+                    0.966 * mainData[i]["失策出"] +
+                    0.865 * mainData[i]["1塁打"] +
+                    1.334 * mainData[i]["2塁打"] +
+                    1.725 * mainData[i]["3塁打"] +
+                    2.065 * mainData[i]["本塁打"]
+                ) / (dasu + shishi + mainData[i]["犠飛"])
+                mainData[i]["WOBA"] = mainData[i]["WOBA"] ? Math.floor(mainData[i]["WOBA"] * 100) / 100 : 0
+
+                mainData[i]["三振率"] = (mainData[i]["三振"] / mainData[i]["打席数"]).toFixed(3)
+                mainData[i]["四球率"] = (mainData[i]["四球"] / mainData[i]["打席数"]).toFixed(3)
+                mainData[i]["進塁打率"] = (mainData[i]["進塁打"] / mainData[i]["走者有"]).toFixed(3)
+
+            }
+            this.calculation_WOBA_avr(mainData)
+            return mainData
+        },
+        calculation_WOBA_avr: function(mainData) {
+            let WOBA_avr = 0
+            for (let i = 0; i < mainData.length; i++) {
+                WOBA_avr += mainData[i]["WOBA"]
+            }
+            WOBA_avr /= mainData.length
+            this.add_WRAA(mainData, WOBA_avr)
+        },
+        add_WRAA: function(mainData, WOBA_avr) {
+            for (let i = 0; i < mainData.length; i++) {
+                mainData[i]["WRAA"] = ((mainData[i]["WOBA"]- WOBA_avr) / 1.24 * mainData[i]["打数"]).toFixed(2)
+            }
+            this.findMax(mainData)
+            return mainData
+        },
+        calculate_tree_ratio: function(anda, dasu) {
+            let k = 0
+            for (k = 0; k <= 4; k++) {
+                    if( ( (anda + k) / (dasu + 4) ) >= 0.3 ) {
+                        return (k + '安打')
+                    }
+                }
+        },
+        findMax: function(mainData) {
+            let dataList = {
+                "game_id": [],
+                "選手名": [],
+                "試合": [],
+                "打席数": [],
+                "打数": [],
+                "1塁打": [],
+                "2塁打": [],
+                "3塁打": [],
+                "本塁打": [],
+                "安打": [],
+                "打点": [],
+                "得点": [],
+                "四球": [],
+                "死球": [],
+                "三振": [],
+                "併殺打": [],
+                "犠飛": [],
+                "犠打": [],
+                "盗塁": [],
+                "盗死": [],
+                "牽制死": [],
+                "失策出": [],
+                "塁打数": [],
+                "打率": [],
+                "出塁率": [],
+                "長打率": [],
+                "OPS": [],
+                "得点圏打率": [],
+                "三振率": [],
+                "四球率": [],
+                "BB/K": [],
+                "XR": [],
+                "XR27": [],
+                "WOBA": [],
+                "WRAA": [],
+                "ゴロアウト": [],
+                "フライアウト": [],
+                "打球": [],
+                "走者有": [],
+                "進塁打": [],
+                "進塁打率": [],
+                "得点圏": [],
+                "ゲッツー崩れ": [],
+                "試合日": [],
+                "三割(4打数)": [],
+                "_cellVariants": []
+            }
+            let i = 0
+            let x = []
+            for (i in mainData) {
+                for (x in mainData[i]) {
+                    dataList[x].push(mainData[i][x])
                 }
             }
-            this.showData = this.showData.filter(v => v)
-            this.showData = statistic(this.showData)
+
+            let maxData = Object.assign({}, dataList);
+            let minData = Object.assign({}, dataList);
+            for (i in maxData) {
+                try{
+                    maxData[i] = Math.max.apply(null, maxData[i])
+                    minData[i] = Math.min.apply(null, minData[i])
+                } catch(e){
+                    maxData[i] = 0
+                    minData[i] = 0
+                }
+            }
+
+            for (let i = 0; i < mainData.length; i++) {
+                let k = 0
+                let items = {}
+                for (k in maxData) {
+                    const reverceData = ['三振', '三振率', '盗塁死', '牽制死', '併殺打']
+                    if (maxData[k] && mainData[i][k] == maxData[k] && !reverceData.includes(k)) {
+                        items[k] = 'info'
+                    } else if (maxData[k] && mainData[i][k] == maxData[k] && reverceData.includes(k)) {
+                        items[k] = 'danger'
+                    } else if (minData[k] && mainData[i][k] == minData[k] && !reverceData.includes(k)) {
+                        items[k] = 'danger'
+                    } else if (minData[k] && mainData[i][k] == minData[k] && reverceData.includes(k)) {
+                        items[k] = 'info'
+                    }
+                    mainData[i]['_cellVariants'] = items
+                }
+            }
+            this.regulationNum = maxData['試合'] * 2
+            return maxData
         }
     }
 };
-
-
-function statistic(mainData) {
-    for (let i = 0; i < mainData.length; i++) {
-        const dasu = mainData[i]["打数"]
-        const shishi = mainData[i]["四球"] + mainData[i]["死球"]
-        mainData[i]["安打"] = mainData[i]["1塁打"] + mainData[i]["2塁打"] + mainData[i]["3塁打"] + mainData[i]["本塁打"]
-        mainData[i]["塁打数"] = mainData[i]["1塁打"] + mainData[i]["2塁打"] * 2 + mainData[i]["3塁打"] * 3 + mainData[i]["本塁打"]* 4
-        mainData[i]["打率"] = (mainData[i]["安打"] / dasu).toFixed(3)
-        mainData[i]["三割(4打数)"] = calculate_tree_ratio(mainData[i]["安打"], dasu)
-        const x = (mainData[i]["安打"] + shishi) / (dasu + shishi + mainData[i]["犠飛"] + mainData[i]["犠打"])
-        mainData[i]["出塁率"] = Math.floor(x * 100) / 100
-        mainData[i]["長打率"] = Math.floor(mainData[i]["塁打数"] / dasu * 100) / 100
-        mainData[i]["OPS"] = (mainData[i]["長打率"] + mainData[i]["出塁率"]).toFixed(3)
-        mainData[i]["BB/K"] = (shishi / mainData[i]["三振"]).toFixed(3)
-        mainData[i]["XR"] = (
-            mainData[i]["1塁打"]
-            + mainData[i]["2塁打"] * 1.44
-            + mainData[i]["3塁打"] * 2.08
-            + mainData[i]["本塁打"] * 2.88
-            + shishi * 0.68
-            + mainData[i]["盗塁"] * 0.36
-            + (dasu - mainData[i]["安打"] - mainData[i]["三振"]) * (-0.18)
-            + mainData[i]["三振"] * (-0.196)
-            + mainData[i]["併殺打"] * (-0.74)
-            + mainData[i]["犠飛"] * 0.74
-            + mainData[i]["犠打"] * 0.08
-            + mainData[i]["牽制死"] * (-0.64)
-        ).toFixed(2)
-
-        mainData[i]["XR27"] = (
-            mainData[i]["XR"] /
-            (dasu -
-            mainData[i]["安打"] +
-            mainData[i]["牽制死"] +
-            mainData[i]["犠飛"] +
-            mainData[i]["犠打"] +
-            mainData[i]["併殺打"]) * 27
-        ).toFixed(2)
-
-        mainData[i]["WOBA"] = (
-            0.692 * mainData[i]["四球"] +
-            0.73 * mainData[i]["死球"] +
-            0.966 * mainData[i]["失策出"] +
-            0.865 * mainData[i]["1塁打"] +
-            1.334 * mainData[i]["2塁打"] +
-            1.725 * mainData[i]["3塁打"] +
-            2.065 * mainData[i]["本塁打"]
-        ) / (dasu + shishi + mainData[i]["犠飛"])
-        mainData[i]["WOBA"] = mainData[i]["WOBA"] ? Math.floor(mainData[i]["WOBA"] * 100) / 100 : 0
-
-        mainData[i]["三振率"] = (mainData[i]["三振"] / mainData[i]["打席数"]).toFixed(3)
-        mainData[i]["四球率"] = (mainData[i]["四球"] / mainData[i]["打席数"]).toFixed(3)
-        mainData[i]["進塁打率"] = (mainData[i]["進塁打"] / mainData[i]["走者有"]).toFixed(3)
-
-    }
-    calculation_WOBA_avr(mainData)
-    return mainData
-}
-
-function calculation_WOBA_avr(mainData) {
-    let WOBA_avr = 0
-    for (let i = 0; i < mainData.length; i++) {
-        WOBA_avr += mainData[i]["WOBA"]
-    }
-    WOBA_avr /= mainData.length
-    add_WRAA(mainData, WOBA_avr)
-}
-
-function add_WRAA(mainData, WOBA_avr) {
-    for (let i = 0; i < mainData.length; i++) {
-        mainData[i]["WRAA"] = ((mainData[i]["WOBA"]- WOBA_avr) / 1.24 * mainData[i]["打数"]).toFixed(2)
-    }
-    findMax(mainData)
-    return mainData
-}
-
-function calculate_tree_ratio(anda, dasu) {
-    let k = 0
-    for (k = 0; k <= 4; k++) {
-            if( ( (anda + k) / (dasu + 4) ) >= 0.3 ) {
-                return (k + '安打')
-            }
-        }
-}
-
-function findMax(mainData) {
-    let dataList = {
-        "game_id": [],
-        "選手名": [],
-        "試合": [],
-        "打席数": [],
-        "打数": [],
-        "1塁打": [],
-        "2塁打": [],
-        "3塁打": [],
-        "本塁打": [],
-        "安打": [],
-        "打点": [],
-        "得点": [],
-        "四球": [],
-        "死球": [],
-        "三振": [],
-        "併殺打": [],
-        "犠飛": [],
-        "犠打": [],
-        "盗塁": [],
-        "盗死": [],
-        "牽制死": [],
-        "失策出": [],
-        "塁打数": [],
-        "打率": [],
-        "出塁率": [],
-        "長打率": [],
-        "OPS": [],
-        "得点圏打率": [],
-        "三振率": [],
-        "四球率": [],
-        "BB/K": [],
-        "XR": [],
-        "XR27": [],
-        "WOBA": [],
-        "WRAA": [],
-        "ゴロアウト": [],
-        "フライアウト": [],
-        "打球": [],
-        "走者有": [],
-        "進塁打": [],
-        "進塁打率": [],
-        "得点圏": [],
-        "ゲッツー崩れ": [],
-        "試合日": [],
-        "三割(4打数)": [],
-        "_cellVariants": []
-    }
-    let i = 0
-    let x = []
-    for (i in mainData) {
-        for (x in mainData[i]) {
-            dataList[x].push(mainData[i][x])
-        }
-    }
-
-    let maxData = Object.assign({}, dataList);
-    let minData = Object.assign({}, dataList);
-    for (i in maxData) {
-        try{
-            maxData[i] = Math.max.apply(null, maxData[i])
-            minData[i] = Math.min.apply(null, minData[i])
-        } catch(e){
-            maxData[i] = 0
-            minData[i] = 0
-        }
-    }
-
-    for (let i = 0; i < mainData.length; i++) {
-        let k = 0
-        let items = {}
-        for (k in maxData) {
-            const reverceData = ['三振', '三振率', '盗塁死', '牽制死', '併殺打']
-            if (maxData[k] && mainData[i][k] == maxData[k] && !reverceData.includes(k)) {
-                items[k] = 'info'
-            } else if (maxData[k] && mainData[i][k] == maxData[k] && reverceData.includes(k)) {
-                items[k] = 'danger'
-            } else if (minData[k] && mainData[i][k] == minData[k] && !reverceData.includes(k)) {
-                items[k] = 'danger'
-            } else if (minData[k] && mainData[i][k] == minData[k] && reverceData.includes(k)) {
-                items[k] = 'info'
-            }
-            mainData[i]['_cellVariants'] = items
-        }
-    }
-    return maxData
-}
-
 </script>
 <style>
 #offence-show table th {
