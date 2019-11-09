04272a51d54e2e12ea9f85402f9e9f530d5ab994
the1031hiroto <crazy_hiroto445@yahoo.co.jp>
2019-11-09 12:03:23 +0900

    update setting

--- src/main.js
+++ src/main.js
@@ -20,13 +20,13 @@ Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 // Initialize Firebase
-var config = {
-  apiKey: "AIzaSyC4YNqGejhAF4FAmpMEYNi-FW7CtWQ6cK0",
-  authDomain: "recor-spa.firebaseapp.com",
-  databaseURL: "https://recor-spa.firebaseio.com",
-  projectId: "recor-spa",
-  storageBucket: "recor-spa.appspot.com",
-  messagingSenderId: "894451295048"
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 firebase.initializeApp(config);
 
